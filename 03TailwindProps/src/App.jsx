3c89098871c084230e13c0c1efe3d6f04f003ee5
Harshil <harshilmayani108@gmail.com>
2025-12-01 00:13:23 +0530

    practice(01/12)

---
+++ 03TailwindProps/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black rounded-l rounded-r'>Tailwind test</h1>
+      <br></br>
+      <Card username="Harshil" btnText="click here"/>
+      <Card username="XYZ" btnText="visit here"/>
+
+      
+    </>
+  )
+}
+
+export default App
